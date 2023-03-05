901c52c3c7fcde2b70913799f83aa084c1edef57
PraiseXI <praisexi@outlook.com>
2023-03-05 01:17:26 +0000

    changes for video display

--- js/Setup/canvasSetup.js
+++ js/Setup/canvasSetup.js
@@ -1,44 +1,115 @@
-let classifier;
-// Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/QQDlg-pFx/';
+async function start() {
+    //TODO: add a loading transition until these setups have been done
 
-// Video
-let video;
-let flippedVideo;
-// To store the classification
-let label = "";
+    displayCameraList()
+    //PASS IN CAMERA ID AND SET TO ACTIVE CAM
+    const currVideo = document.getElementById('video-element');
+    const currentCameraID = localStorage.getItem('cam-ID');
+    let currentCamera = await getDeviceByID(currentCameraID);
+    const cameraList = document.getElementById('webcam-list');
+    stopRunningVideoStreams();
+    setSelectedOption(cameraList, currentCamera.label);
+    setExistingVideoStream(currentCameraID, currVideo);
 
-// Load the model first
-function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+    cameraList.addEventListener('change', displayVideoStream);
 }
 
-function setup() {
-  console.clear()
-  createCanvas(800, 650);
-  resultP = createP('waiting...');
-  resultP.style('font-size', '32pt');
-  // Create the video
-  video = createCapture(VIDEO);
-  video.size(800, 650);
-  video.hide();
-
-  flippedVideo = ml5.flipImage(video);
-  // Start classifying
-  classifyVideo();
+function stopRunningVideoStreams() {
+    // to shut all the open or running video streams
+    if (window.videoStream !== null && window.videoStream !== undefined && window.videoStream.length > 0 &&
+        video) {
+        window.videoStream.forEach((stream) => {
+            if (stream) {
+                if ('getTracks' in stream && stream.getTracks().length > 0) {
+                    stream.getTracks().forEach((track) => stopTrack(track));
+                }
+                if ('getVideoTracks' in stream && stream.getVideoTracks().length > 0) {
+                    stream.getVideoTracks().forEach((track) => stopTrack(track));
+                }
+                if ('getAudioTracks' in stream && stream.getAudioTracks().length > 0) {
+                    stream.getAudioTracks().forEach((track) => stopTrack(track));
+                }
+            }
+        });
+        window.videoStream = [];
+        video.srcObject = null;
+    }
 }
 
-function draw() {
-  //hide display
-  background(0);
-  // Draw the video
-  image(flippedVideo, 0, 0);
-  
-  /*
-   // Draw the label
-   fill(100);
-   textSize(50);
-   textAlign(CENTER);
-   text(label, width / 2, height - 4);
-   */
+function setExistingVideoStream(deviceID, currVideo) {
+    //create video with existing deviceID
+    navigator.mediaDevices.getUserMedia({
+        video: {
+            deviceId: { exact: deviceID }
+        }
+    }).then(stream => {
+        currVideo.srcObject = stream;
+        currVideo.play();
+    }).catch(error => {
+        console.error(error);
+    });
+
+    document.body.appendChild(currVideo);
 }
+
+async function getDeviceByID(deviceId) {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevice = devices.find(device => device.kind === 'videoinput' && device.deviceId === deviceId);
+
+    return videoDevice;
+}
+
+function setSelectedOption(selectList, value) {
+    const options = selectList.options;
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].label === value) {
+            selectList.selectedIndex = i;
+            break;
+        }
+    }
+}
+
+function clearSelectList(List) {
+    while (selectElement.options.length > 0) {
+        selectElement.remove(0);
+      }
+}
+
+function displayCameraList() {
+    // Get a list of all connected devices that are webcams
+    navigator.mediaDevices.enumerateDevices()
+        .then(devices => {
+            const webcams = devices.filter(device => device.kind === 'videoinput');
+
+            // Display the list of webcams
+            const webcamList = document.getElementById('webcam-list');
+            webcams.forEach(webcams => {
+                const option = document.createElement('option');
+                option.value = webcams.deviceId;
+                option.text = webcams.label;
+                webcamList.appendChild(option);
+            });
+
+        });
+}
+
+export function displayVideoStream() {
+    // Get the selected camera device
+    const selectedDeviceId = document.getElementById('webcam-list').value;
+
+    // Request access to the camera device
+    navigator.mediaDevices.getUserMedia({ video: { deviceId: selectedDeviceId } })
+        .then(stream => {
+            // Get the video element
+            const videoElement = document.getElementById('video-element');
+
+            // Display the video stream in the video element
+            videoElement.srcObject = stream;
+            videoElement.play();
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+start();
