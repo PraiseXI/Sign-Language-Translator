c7fa0378cd46056bb04e7d92541351ae473eb247
PraiseXI <praisexi@outlook.com>
2023-03-09 22:36:39 +0000

    output translation page

---
+++ output.js
@@ -0,0 +1,15 @@
+const output = document.getElementById("translation");
+const sentence = localStorage.getItem('sentence')
+
+function restart() {
+    window.location.href = 'startPage.html';
+}
+function setTranslation() {
+    output.innerHTML = sentence
+}
+setTranslation()
+const restartButton = document.getElementById('restart');
+restartButton.addEventListener('click', restart);
+
+//TODO: DO IT IN DIFFERENT LANGUAGES AND SPEECH
+//TODO: TALKING PERSON?
